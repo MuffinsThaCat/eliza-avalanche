6dd25417b940c4a37eaf6072308e07a0570a193a
MuffinsThaCat <talzisckind19@gmail.com>
2024-12-13 01:58:43 -0500

    Update environment.ts

--- packages/client-pinecone/src/environment.ts
+++ packages/client-pinecone/src/environment.ts
@@ -4,3 +4,14 @@ export interface PineconeEnvironment {
     PINECONE_INDEX_NAME: string;
     OPENAI_API_KEY: string;
 }
+
+export function validatePineconeEnvironment(env: Partial<PineconeEnvironment>): boolean {
+    const requiredVars: (keyof PineconeEnvironment)[] = [
+        'PINECONE_API_KEY',
+        'PINECONE_ENVIRONMENT',
+        'PINECONE_INDEX_NAME',
+        'OPENAI_API_KEY'
+    ];
+    
+    return requiredVars.every(variable => !!env[variable]);
+}
